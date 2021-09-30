693c44caded01792a50b4ba3e4b12a34c4a4a7f1
klugh <klugh@student.ethz.ch>
2021-09-30 08:58:06 +0200

    update launch model training

--- prepare_thesis/launch_model_training.py
+++ prepare_thesis/launch_model_training.py
@@ -6,14 +6,26 @@ from mmvae_hub.utils.utils import json2dict, dict2json
 
 end_epoch = 500
 eval_freq = 1000
-nbr_repeats = 1
+nbr_repeats = 5
+
+poe_args = {
+    'method': 'poe',
+    "initial_learning_rate": 0.0005,
+    'class_dim': 1280,
+    "min_beta": 0,
+    "max_beta": 2.0,
+    "beta_warmup": 0,
+    "num_mods": 3,
+    "end_epoch": end_epoch,
+    "eval_freq": eval_freq,
+}
 
 moe_args = {
     'method': 'moe',
-    "initial_learning_rate": 0.001,
-    'class_dim': 512,
+    "initial_learning_rate": 0.0005,
+    'class_dim': 1280,
     "min_beta": 0,
-    "max_beta": 2.5,
+    "max_beta": 2.0,
     "beta_warmup": 0,
     "num_mods": 3,
     "end_epoch": end_epoch,
@@ -22,10 +34,10 @@ moe_args = {
 
 mopoe_args = {
     'method': 'mopoe',
-    "initial_learning_rate": 0.001,
-    'class_dim': 512,
+    "initial_learning_rate": 0.0005,
+    'class_dim': 1280,
     "min_beta": 0,
-    "max_beta": 2.5,
+    "max_beta": 2.0,
     "beta_warmup": 0,
     "num_mods": 3,
     "end_epoch": end_epoch,
@@ -37,31 +49,30 @@ mopgfm_args = {
     "initial_learning_rate": 0.0005,
     'class_dim': 1280,
     "min_beta": 0,
-    "max_beta": 2.0,
+    "max_beta": 1.4,
     "beta_warmup": 50,
     "num_mods": 3,
     "end_epoch": end_epoch,
     "eval_freq": eval_freq,
-    "coupling_dim": 64,
-    "num_gfm_flows": 1,
-    "nbr_coupling_block_layers": 5
-
-}
-
-mogfm_args = {
-    'method': 'iwmogfm',
-    "initial_learning_rate": 0.0005,
-    'class_dim': 640,
-    "min_beta": 0,
-    "max_beta": 2.0,
-    "beta_warmup": 50,
-    "num_mods": 3,
-    "end_epoch": end_epoch,
-    "eval_freq": eval_freq,
-    "coupling_dim": 32,
+    "coupling_dim": 128,
     "num_gfm_flows": 3,
-
+    "nbr_coupling_block_layers": 8
 }
+
+# mogfm_args = {
+#     'method': 'iwmogfm',
+#     "initial_learning_rate": 0.0005,
+#     'class_dim': 640,
+#     "min_beta": 0,
+#     "max_beta": 2.0,
+#     "beta_warmup": 50,
+#     "num_mods": 3,
+#     "end_epoch": end_epoch,
+#     "eval_freq": eval_freq,
+#     "coupling_dim": 32,
+#     "num_gfm_flows": 3,
+#
+# }
 
 if __name__ == '__main__':
     experiment_uids_path = Path(__file__).parent.parent / 'data/thesis/experiment_uids.json'
@@ -80,15 +91,16 @@ if __name__ == '__main__':
             params['num_mods'] = num_mods
             # more evaluation steps are needed for 3 mods
             if num_mods == 3:
-                params['eval_freq'] = 50
+                params['eval_freq'] = 100
             exp_uids[method][f'{num_mods}_mods'] = []
 
             for _ in range(nbr_repeats):
                 experiment_uid = get_experiment_uid('polymnist', method=method)
                 params["experiment_uid"] = experiment_uid
+                params['dir_experiment'] = str(Path('~/klugh/mmvae_hub_data/experiments/mmnf_repsep').expanduser())
 
                 exp_uids[method][f'{num_mods}_mods'].append(experiment_uid)
-                # todo change experiment_dir path
+
                 launch_leomed_jobs(which_dataset='polymnist', params=params)
 
     dict2json(experiment_uids_path, d=exp_uids)
---
+++ prepare_thesis/make_rand_gen_plots.py
@@ -0,0 +1,43 @@
+import random
+from pathlib import Path
+
+import torch
+from matplotlib import pyplot as plt
+from mmvae_hub.experiment_vis.utils import load_experiment
+from mmvae_hub.utils.plotting import plot
+from modun.file_io import json2dict
+
+data_dir = Path(__file__).parent.parent / 'data/thesis'
+exp_uids = json2dict(data_dir / 'experiment_uids.json')
+
+num_samples = 30
+
+for method in ['mopoe', 'mopgfm', 'moe']:
+    save_path = data_dir / f'{method}/rand_gen_plot.png'
+
+    _id = exp_uids[method]['3_mods'][0]
+    experiment_dir = data_dir / _id
+
+    exp = load_experiment(experiment_dir=experiment_dir, _id=_id)
+
+    model = exp.mm_vae
+    mods = exp.modalities
+    random_samples = model.generate(num_samples)
+    m_keys = list(mods.keys())
+
+    rec = torch.zeros(exp.plot_img_size,
+                      dtype=torch.float32).repeat(num_samples, 1, 1, 1)
+    for l in range(num_samples):
+        m_key_in = random.choice(m_keys)
+        mod = mods[m_key_in]
+        samples_mod = random_samples[m_key_in]
+        rand_plot = mod.plot_data(samples_mod[l])
+        rec[l, :, :, :] = rand_plot
+
+    fig = plot.create_fig('', rec, 10, save_figure=False)
+
+    plt.figure(figsize=(10, 10))
+    plt.imshow(fig)
+    plt.axis('off')
+    plt.savefig(save_path, bbox_inches='tight', pad_inches=0)
+    plt.close()
