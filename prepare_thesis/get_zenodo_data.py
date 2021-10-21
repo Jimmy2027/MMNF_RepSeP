121478121426784bfb7af82da3490070b66373e7
klugh <klugh@student.ethz.ch>
2021-10-21 15:55:51 +0200

    update

--- prepare_thesis/get_zenodo_data.py
+++ prepare_thesis/get_zenodo_data.py
@@ -4,4 +4,4 @@ from pathlib import Path
 from modun.download_utils import download_zip_from_url
 
 download_zip_from_url(url="https://zenodo.org/record/5588294/files/data.zip?download=1",
-                      dest_folder=Path(__file__).parent / 'data', verbose=True)
+                      dest_folder=Path(__file__).parent.parent, verbose=True)
